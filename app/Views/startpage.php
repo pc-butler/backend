029c66b7beb89d1943fd9626591e3b3017ce2dd5
Royce <roycewhitaker@rocketmail.com>
2021-04-21 20:46:50 -0700

    Add files via upload

---
+++ app/Views/startpage.php
@@ -0,0 +1,325 @@
+		<div class="wrapper wrapper-content animated fadeIn">
+			<div id="content" style="opacity: 1;">
+	
+		<style>
+			#content{
+				font-size: 21px;
+			}
+			
+			#Title {
+				font-family: 'Malgun Gothic';
+				text-align: center;
+				color: #333;
+				margin-bottom: 20px;
+			}
+			
+			#searchbox{
+
+				width: 500;
+				height: 4%;
+				border: none;
+				border-radius: 2px;
+				outline: none;
+				padding-left: 15px;
+				text-align: left;
+				background-color: white;
+				color: #333;
+				font-size: 15px;
+				font-family: 'Malgun Gothic', sans-serif;
+				display: block;
+				margin: auto;
+				margin-top: 50px;
+			}
+			
+		    #stripe {
+		        width:100%;
+				vertical-align:middle;
+		    }
+
+			#mainframe {
+			    text-align: center;
+		/* 	    position: absolute; */
+			    top: 16%;
+			    left: 16%;
+			    right: 16%;
+			    display: flex;
+			    flex-flow: wrap;
+			    justify-content: space-around;
+			    max-width: 1400px;
+			    margin: auto;
+			}
+			
+			#footer {
+				position: absolute;
+				bottom : 0;
+				right: 0;
+				text-align: right;
+				padding: 10px;
+				font-size:30%;
+			}
+			
+			#linkblock {
+				width: 250px;
+				min-height:200px;
+		/* 		margin-left:25px;
+				margin-right:30px;
+				margin-top:30px;
+				margin-bottom:40px; */
+				padding-left:67px;
+				padding-right:1px;
+				padding-top:0px;
+				padding-bottom:5px;
+				color:#000;
+				text-align: left;
+				background-position:top 0px left 0px;; 
+				background-repeat:no-repeat;
+				background-size: 50px 150px;
+				font-family: 'Malgun Gothic';
+				font-size:100%;
+				display: inline-block;
+				vertical-align: top;
+				border-left: 3px solid #607d8b;
+				
+				
+		/* 		width: 100px;  /* Or whatever */ */
+		/*   		height: 100px; /* Or whatever */ */
+		  		margin: auto;  /* Magic! */
+				flex-basis: 15%;
+		    }
+			
+			
+			#linkblock ul { 
+				list-style-type: none;
+				padding-left: 0;
+				margin: auto;
+				width: 180px;
+
+			} 
+			
+			a:link { text-decoration:none; font-weight:normal; color:#333; }
+			a:visited { text-decoration:none; font-weight:normal; color:#333; }
+			a:hover { text-decoration:none; font-weight:normal; color:#333; }
+			a:active { text-decoration:none; font-weight:normal; color:#333; }
+			a:focus { text-decoration:none; font-weight:normal; color:#333; }
+			
+			#footer a:link { text-decoration:none; font-weight:normal; color:#101010; }
+			#footer a:visited { text-decoration:none; font-weight:normal; color:#101010; }
+			#footer a:hover { text-decoration:none; font-weight:normal; color:#101010; }
+			#footer a:active { text-decoration:none; font-weight:normal; color:#101010; }
+			#footer a:focus { text-decoration:none; font-weight:normal; color:#101010; }
+
+			@media only screen and (max-width: 999px) {
+				#mainframe {
+				    left: 0%;
+				    right: 0%;
+				}
+			}
+			.below{
+				background-color: white;
+				width: 90%;
+				height: 100vh;
+				margin: 0 auto;
+				color: maroon;
+				display: flex;
+				flex-flow: wrap;
+				justify-content: space-evenly;
+			}
+			.item {
+				flex-basis: auto;
+				width: 300px;
+				max-height: 600px;
+			}
+			@media all and (max-width: 600px) {
+				#mainframe {
+					top: 20%;
+					left: 0%;
+				}
+				#linkblock {
+					margin-top: 0px;
+				}
+				#searchbox {
+					display: none;
+				}
+				#Title {
+					margin-top: 0px;
+				}
+			}
+
+		</style>
+        	<title>** Homepage **</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	
+	<div id="stripe">
+		<div id="Title"> Loading.. </div>
+<!-- 			<input id="searchbox" placeholder="Options: | -a | -h | -m | -w | -y |" type="text" autofocus="false"  autocomplete="true"></input> -->
+			<div id="mainframe">
+				<div id="linkblock" style="background-image:url(https://lunarcontrol.github.io/startpage/star-homepage/images/planet.png)">
+					<ul>
+						<li><b>School</b></li>
+						<li><a href="https://one.sjsu.edu/">one.SJSU</a></li>
+						<li><a href="https://www.github.com/lunarcontrol">Github/lunarcontrol</a></li>
+						<li><a href="https://www.gmail.com">Gmail</a> // <a href="https://mail.yahoo.com">Ymail</a></li>
+						<li><a href="https://drive.google.com">Drive</a></li>
+						<li><a href="https://calendar.google.com">Google Calendar</a></li>
+						<li><a href="https://messages.google.com/web/conversations">Google Messages</a></li>
+					</ul>
+				</div>
+				<div id="linkblock" style="background-image:url(https://lunarcontrol.github.io/startpage/star-homepage/images/flowers.png);">
+					<ul>
+						<li><b>Streaming</b></li>
+						<li><a href="https://airhq.site">airhq.site</a></li>
+						<li><a href="https://hulu.com">Hulu</a></li>
+						<li><a href="https://app.plex.tv/desktop">Plex</a></li>
+						<li><a href="https://1337x.to/">1337x</a></li>
+					</ul>
+				</div>
+				<div id="linkblock" style="background-image:url(https://lunarcontrol.github.io/startpage/star-homepage/images/logs.png);">
+					<ul>
+						<li><b>Social</b></li>
+						<li><a href="https://youtube.com">YouTube</a></li>
+						<li><a href="https://reddit.com">Reddit</a></li>
+						<li><a href="https://www.4chan.org">4chan</a></li>
+						<li><a href="https://twitter.com">Twitter</a></li>
+						<li><a href="https://riot.theguards.net/#/room/#general:theguards.net">Riot - The Guards</a></li>
+						<li><a href="https://discord.com">Discord</a></li>
+						<li><a href="https://twitch.tv">Twitch</a></li>
+						<li><a href="http://www.facebook.com">Facebook</a></li>
+						<li><a href="http://www.linkedin.com">LinkedIn</a></li>
+					</ul>
+				</div>
+				<div id="linkblock" style="background-image:url(https://lunarcontrol.github.io/startpage/star-homepage/images/blossom.png);">
+					<ul>
+						<li><b>Language</b></li>
+						<li><a href="https://play.google.com/books">Google Books</a></li>
+						<li><a href="https://www.duolingo.com/">Duolingo</a></li>
+						<li><a href="https://app.languagedrops.com/">LanguageDrops</a></li>
+						<li><a href="https://mangolanguages.com/">MangoLanguages</a></li>
+						<li><a href="https://www.tofugu.com/learn-japanese/">Tofugo JP</a></li>
+					</ul>
+				</div>
+				<div id="linkblock" style="background-image:url(https://lunarcontrol.github.io/startpage/star-homepage/images/malachite.png);">
+					<ul>
+						<li><a style="color: black;" href="https://torn.com"><b>Torn</b></a></li>
+						<li><a href="https://lunarcontrol.github.io/Eazy-T/">Eazy-T</a></li>
+						<li><a href="https://yata.yt/">YATA</a></li>
+						<li>API: BcB8aMys7lncbQCi</li>
+					</ul>
+				</div>
+				<div id="linkblock" style="background-image:url(https://lunarcontrol.github.io/startpage/star-homepage/images/sway.png);">
+					<ul>
+						<li><b>J-Resources</b></li>
+						<li><a href="http://mov3.co/en/">mov3</a></li>
+						<li><a href="https://abema.tv/">Abema</a></li>
+						<li><a href="https://www.japanesepod101.com/">Japanese Pod</a></li>
+						<li><a href="https://getplan.co/">Get Plan</a></li>
+						<li><a href="https://j-talk.com/convert">J-Talk</a></li>
+						<li><a href="https://sethclydesdale.github.io/genki-study-resources/lessons-3rd/">Genki Study</a></li>
+						<li><a href="https://www.wanikani.com/">Wanikani</a></li>
+						<li><a href="https://jisho.org/">Jisho</a></li>
+						<li><a href="https://www.japanesepod101.com/japanese-dictionary/">J-Pod Dictionary</a></li>
+					</ul>
+				</div>
+				<div id="linkblock" style="background-image:url(https://lunarcontrol.github.io/startpage/star-homepage/images/sway.png);">
+					<ul>
+						<li><a style="color: black;" href="https://www.reddit.com/user/sparky1216/m/j_subs/"><b>J-Subreddits</b></a></li>
+						<li><a href="https://www.reddit.com/r/japan/">r/Japan</a></li>
+						<li><a href="https://www.reddit.com/r/LearnJapanese/">r/LearnJapanese</a></li>
+						<li><a href="https://www.reddit.com/r/japanlife/">r/JapanLife</a></li>
+						<li><a href="https://www.reddit.com/r/teachinginjapan/">r/TeachingInJapan</a></li>
+						<li><a href="https://www.reddit.com/r/japaneseresources/">r/JapaneseResources</a></li>
+						<li><a href="http://jpopsuki.fm:2199/tunein/-stream/jpopsuki.pls">Jpop Suki Radio</a></li>
+					</ul>
+				</div>
+				<div id="linkblock" style="background-image:url(https://lunarcontrol.github.io/startpage/star-homepage/images/sway.png);">
+					<ul>
+						<li><b>Torrent</b></li>
+						<li><a href="https://lunarctrl.cloud.seedboxes.cc/rutorrent/">Seedbox</a></li>
+						<li><a href="https://avistaz.to/">AvistaZ</a></li>
+						<li><a href="http://asiancinema.me/">Asiancinema</a></li>
+						<li><a href="https://nyaa.si/">Nyaa</a></li>
+						<li><a href="http://exoticaz.to/">Exoticaz</a></li>
+						<li><a href="https://retroflix.club/">Retroflix</a></li>
+						<li><a href="https://aither.cc/">Aither</a></li>
+						<li><a href="https://www.torrentleech.org/">TorrentLeech</a></li>
+						<li><a href="https://www.scenetime.com/">SceneTime</a></li>
+						<li><a href="https://torrentdb.net/">TorrentDB</a></li>
+					</ul>
+				</div>
+				<div id="linkblock" style="background-image:url(https://lunarcontrol.github.io/startpage/star-homepage/images/sway.png);">
+					<ul>
+						<li><a style="color: black;" href="https://www.reddit.com/user/sparky1216/m/meme_sub/"><b>Meme Subs</b></a></li>
+						<li><a href="https://www.reddit.com/r/HistoryMemes/">r/HistoryMemes</a></li>
+						<li><a href="https://www.reddit.com/r/ihaveihaveihavereddit/">r/ihaveihaveihavereddit</a></li>
+						<li><a href="https://www.reddit.com/r/okbuddyretard/">r/okbuddyretard</a></li>
+						<li><a href="https://www.reddit.com/r/gangweed/">r/GangWeed</a></li>
+						<li><a href="https://www.reddit.com/r/BanVideoGames/">r/BanVideoGames</a></li>
+						<li><a href="https://www.reddit.com/r/Animemes/">r/Animemes</a></li>
+					</ul>
+				</div>
+				<div id="linkblock" style="background-image:url(https://lunarcontrol.github.io/startpage/star-homepage/images/anime.png);">
+					<ul>
+						<li><b>Anime</b></li>
+						<li><a href="https://myanimelist.net/">MyAnimeList</a></li>
+					</ul>
+				</div>
+				<div id="linkblock" style="background-image:url(https://lunarcontrol.github.io/startpage/star-homepage/images/flowers.png);">
+					<ul>
+						<li><b>Financial</b></li>
+						<li><a href="https://robinhood.com/">Robinhood</a></li>
+						<li><a href="https://comfirstcu.org">Comfirst</a></li>
+						<li><a href="https://discover.com">Discover</a></li>
+						<li><a href="https://www.chase.com/">Chase</a></li>
+						<li><a href="https://xfinity.com">Xfinity</a></li>
+					</ul>
+				</div>
+			</div>
+		<style>
+			.item ul {
+				margin-top: 25px;
+			}
+			
+		</style>
+		<div class="below">
+			<div class="item">
+				<h1>Daily<br></h1>
+				<ul>
+					<li><a href="https://docs.google.com/spreadsheets/d/1aSCRHgVmI6EhApngy-IaMzJUJRk2yZpJ_rhjNytJD64/edit?usp=sharing">Habit Tracker</a></li>
+				</ul>
+			</div>
+			<div class="item">
+				<ul>
+					<li><b>Study Abroad</b></li>
+					<li><a href="https://clscholarship.org/apply">Critical Language Scholarship</a></li>
+					<li><a href="https://us.fulbrightonline.org/fulbright-us-student-program">Fullbright</a> <a href="https://us.fulbrightonline.org/countries/selectedprogram/38"> [Japan]</a></li>
+					<li><a href="https://www.iie.org/en/Programs/Freeman-ASIA/">Freeman Asia</a></li>
+					<li><a href="https://www.gooverseas.com/">Go Overseas</a></li>
+					<li><a href="https://www.jasso.go.jp/en/index.html">Japan Student Services Org. (JASSO) Scholarship</a></li>
+					<li><a href="https://jetprogramusa.org/application-departure/">JET Application</a></li>
+				</ul>
+			</div>
+			<div class="item">
+				<iframe width="100%" height="600px" frameborder="0" scrolling="no" src="https://lunarcontrol.github.io/Eazy-T">iFrame Broken!</iframe>
+			</div>
+		</div>
+	
+<!-- 	<script src="https://lunarcontrol.github.io/startpage/star-homepage/search.js" type="text/javascript"></script> -->
+
+</div></div>
+		</div>
+
+
+		<script>
+			fetch("/startpage/quotes.json").then(function(response) {return response.json();})
+	        .then(function(data) {
+	            data = data[Math.floor(Math.random()*data.length)];
+	            console.log(data);
+	            if(data.author == null){
+	                titleText = data.text + "<br>- unknown";
+	            }
+	            else{
+	                titleText = data.text + " <br>- " + data.author;
+	            }
+	            document.getElementById("Title").innerHTML = titleText;
+	        });
+		</script>
