c13dd8a45abdc960dd05c88d4218df44a131a915
Royce <roycewhitaker@rocketmail.com>
2021-04-28 20:12:34 -0700

    Update footer.php

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -9,44 +9,44 @@
 
     <!-- Mainly scripts -->
     
-    <script type="javascript" src="/js/plugins/metisMenu/jquery.metisMenu.js"></script>
-    <script type="javascript" src="/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+    <script type="text/javascript" src="/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script type="text/javascript" src="/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
 
     <!-- Flot -->
-    <script type="javascript" src="/js/plugins/flot/jquery.flot.js"></script>
-    <script type="javascript" src="/js/plugins/flot/jquery.flot.tooltip.min.js"></script>
-    <script type="javascript" src="/js/plugins/flot/jquery.flot.spline.js"></script>
-    <script type="javascript" src="/js/plugins/flot/jquery.flot.resize.js"></script>
-    <script type="javascript" src="/js/plugins/flot/jquery.flot.pie.js"></script>
+    <script type="text/javascript" src="/js/plugins/flot/jquery.flot.js"></script>
+    <script type="text/javascript" src="/js/plugins/flot/jquery.flot.tooltip.min.js"></script>
+    <script type="text/javascript" src="/js/plugins/flot/jquery.flot.spline.js"></script>
+    <script type="text/javascript" src="/js/plugins/flot/jquery.flot.resize.js"></script>
+    <script type="text/javascript" src="/js/plugins/flot/jquery.flot.pie.js"></script>
 
     <!-- Peity -->
-    <script type="javascript" src="/js/plugins/peity/jquery.peity.min.js"></script>
-    <script type="javascript" src="/js/demo/peity-demo.js"></script>
+    <script type="text/javascript" src="/js/plugins/peity/jquery.peity.min.js"></script>
+    <script type="text/javascript" src="/js/demo/peity-demo.js"></script>
 
     <!-- Custom and plugin javascript -->
-    <script type="javascript" src="/js/inspinia.js"></script>
-    <script type="javascript" src="/js/plugins/pace/pace.min.js"></script>
+    <script type="text/javascript" src="/js/inspinia.js"></script>
+    <script type="text/javascript" src="/js/plugins/pace/pace.min.js"></script>
 
     <!-- jQuery UI -->
-    <script type="javascript" src="/js/plugins/jquery-ui/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="/js/plugins/jquery-ui/jquery-ui.min.js"></script>
 
     <!-- GITTER -->
-    <script type="javascript" src="/js/plugins/gritter/jquery.gritter.min.js"></script>
+    <script type="text/javascript" src="/js/plugins/gritter/jquery.gritter.min.js"></script>
 
     <!-- Sparkline -->
-    <script type="javascript" src="/js/plugins/sparkline/jquery.sparkline.min.js"></script>
+    <script type="text/javascript" src="/js/plugins/sparkline/jquery.sparkline.min.js"></script>
 
     <!-- Sparkline demo data  -->
-    <script type="javascript" src="/js/demo/sparkline-demo.js"></script>
+    <script type="text/javascript" src="/js/demo/sparkline-demo.js"></script>
 
     <!-- ChartJS-->
-    <script type="javascript" src="/js/plugins/chartJs/Chart.min.js"></script>
+    <script type="text/javascript" src="/js/plugins/chartJs/Chart.min.js"></script>
 
     <!-- Toastr -->
-    <script type="javascript" src="/js/plugins/toastr/toastr.min.js"></script>
+    <script type="text/javascript" src="/js/plugins/toastr/toastr.min.js"></script>
 
     <!-- Datatables -->
-    <script type="javascript" src="/js/plugins/dataTables/datatables.min.js"></script>
+    <script type="text/javascript" src="/js/plugins/dataTables/datatables.min.js"></script>
 
 
     <script>
