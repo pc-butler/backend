d396d990157fbdfafafddf3d1564356a5e1598c0
Royce <roycewhitaker@rocketmail.com>
2021-04-28 19:57:12 -0700

    Update index.php

--- app/Views/pages/index.php
+++ app/Views/pages/index.php
@@ -1,8 +1,10 @@
-<h1>eHealth-Calendar</h1>
+<div class="row  border-bottom white-bg dashboard-header animated fadeIn">
+  <h1>eHealth-Calendar</h1>
 
-<h2>Features:</h2>
+  <h2>Features:</h2>
 
-<ul>
-  <li>Keep track of routine medicine intake.</li>
-  <li>Upload medical records to an unsecure server!</li>
-</ul>
+  <ul>
+    <li>Keep track of routine medicine intake.</li>
+    <li>Upload medical records to an unsecure server!</li>
+  </ul>
+</div>
